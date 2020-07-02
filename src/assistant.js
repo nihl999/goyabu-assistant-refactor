7feb4c5940d0138c3483ab33e08f0b300e8a650c
Arthur Lorhan <arthurlohransilva@gmail.com>
2020-07-01 21:52:25 -0300

    Add basic render function: TODO work in all pages

--- src/assistant.js
+++ src/assistant.js
@@ -1,14 +1,25 @@
-/* function addFavs(){
-    const favs = getFavs();
-    const newAnime = getInfo();
-    favs.push(newAnime);
-    localStorage.setItem('favorites', JSON.stringify(favs));
-}; */
+function showFavs(){
+    document.querySelector('#home-content').innerHTML = '';
+    chrome.storage.sync.get(['favorites'], function(favs) {
+        favs.favorites.forEach(anime => {
+            const animeRegion = document.querySelector('#home-content');
+            const animeName = anime.nome;
+            const animeLabel = document.createTextNode( animeName + '\n');
+            const animeURL = anime.link;
+            const animeNode = document.createElement('a');
+            animeNode.setAttribute('href', animeURL);
+            animeNode.appendChild(animeLabel);
+            animeRegion.appendChild(animeNode);
+    
+        });
+        console.log(favs);
+    });
+};
 function addFavs(){
     const newAnime = getInfo();
     chrome.storage.sync.get(['favorites'], function(favs) {
         if(favs.favorites.some(anime => anime.nome === newAnime.nome) == false)
-            favs.favorites.push(newAnime);
+        favs.favorites.push(newAnime);
         console.log(favs);
         chrome.storage.sync.set({favorites: favs.favorites});
     });
@@ -23,9 +34,6 @@ function getInfo(){
     }
     return animeData;
 }
-function clearHome(){
-    document.querySelector('#home-content').remove();
-}
 const favList = () => new Promise((resolve) => {
     if (document.readyState == 'interactive' || document.readyState == 'complete'){
         const sideBar = document.querySelector('.sidebar-nav');
@@ -35,7 +43,7 @@ const favList = () => new Promise((resolve) => {
         assistant.querySelector('a').removeAttribute('href');
         assistant.querySelector('a').childNodes[1].nodeValue = 'Goyabu Assistant';
         assistant.querySelector('.material-icons').innerText = 'video_library';
-        assistant.addEventListener('click', clearHome);
+        assistant.addEventListener('click', showFavs);
         sideBarList.appendChild(assistant);
     }
 });
